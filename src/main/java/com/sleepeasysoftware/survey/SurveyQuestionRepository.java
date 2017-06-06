e9be159456229b80b2d0153554aedd428e5a176d
Pivotal <pivotal@silverlake.la.pivotallabs.com>
2017-06-06 16:34:00 -0700

    Git ignore

--- src/main/java/com/sleepeasysoftware/survey/SurveyQuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sleepeasysoftware.survey;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "surveyQuestion", exported = false)
-public interface SurveyQuestionRepository extends PagingAndSortingRepository<SurveyQuestion, Long> {
-}
